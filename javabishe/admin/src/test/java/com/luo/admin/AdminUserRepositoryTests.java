c03450c609fd7aec2ca3a9e4959b95504f82e91e
luo <free1193984561@126.com>
2018-12-25 15:38:41 +0800

    测试

---
+++ javabishe/admin/src/test/java/com/luo/admin/AdminUserRepositoryTests.java
@@ -0,0 +1,29 @@
+package com.luo.admin;
+
+
+import com.luo.core.entitys.AdminUser;
+import com.luo.core.repositories.AdminUserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminUserRepositoryTests {
+
+    @Autowired
+    AdminUserRepository adminUserRepository;
+
+    /**
+     * 测试成功
+     */
+    @Test
+    public void findAdminUserByNameOrUsernameOrEmailOrPhone(){
+        AdminUser adminUser = adminUserRepository.
+                findAdminUserByNameOrUsernameOrEmailOrPhone("123",
+                        "","","");
+        System.out.println(adminUser);
+    }
+}
