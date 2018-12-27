116e5a88bcb80539e7974553a70861fa7f9bb200
luo <free1193984561@126.com>
2018-12-27 14:53:23 +0800

    修改过滤状态

--- javabishe/core/src/main/java/com/luo/core/specification/MySpecification.java
+++ javabishe/core/src/main/java/com/luo/core/specification/MySpecification.java
@@ -10,8 +10,8 @@ public class MySpecification<T> implements Specification<T> {
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
         Path<Integer> _checkStatus = root.get("checkStatus");
         Path<Integer> _showStatus = root.get("showStatus");
-        Predicate _key1 = criteriaBuilder.equal(_showStatus, 1);
-        Predicate _key2 = criteriaBuilder.equal(_checkStatus, 1);
+        Predicate _key1 = criteriaBuilder.equal(_showStatus, 0);
+        Predicate _key2 = criteriaBuilder.equal(_checkStatus, 0);
         return criteriaBuilder.and(_key1, _key2);
     }
 }
