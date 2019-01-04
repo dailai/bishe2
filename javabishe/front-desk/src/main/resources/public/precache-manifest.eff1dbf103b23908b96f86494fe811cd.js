188d98023865c886ad2c37042e2950ad1093f046
luo <free1193984561@126.com>
2019-01-04 09:56:44 +0800

    前端打包项目

---
+++ javabishe/front-desk/src/main/resources/public/precache-manifest.eff1dbf103b23908b96f86494fe811cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "78023ef13cc38d664f924c5f664c89a0",
+    "url": "./static/media/videolb3.78023ef1.jpg"
+  },
+  {
+    "revision": "6841c31527b2b83c8eaa25055a08a6a9",
+    "url": "./static/media/videolb2.6841c315.jpg"
+  },
+  {
+    "revision": "9d55470e6a0ee9e600a6939dcd379fe9",
+    "url": "./static/media/videolb1.9d55470e.jpg"
+  },
+  {
+    "revision": "b353b68c3ee456d006ca0f338f2153cb",
+    "url": "./static/media/icons.b353b68c.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "75736fa5469efd18b46d",
+    "url": "./static/js/main.75736fa5.chunk.js"
+  },
+  {
+    "revision": "a80c2d736712702e4acf",
+    "url": "./static/js/1.a80c2d73.chunk.js"
+  },
+  {
+    "revision": "75736fa5469efd18b46d",
+    "url": "./static/css/main.24d60678.chunk.css"
+  },
+  {
+    "revision": "a80c2d736712702e4acf",
+    "url": "./static/css/1.cce0fdb6.chunk.css"
+  },
+  {
+    "revision": "ba8197afec8a9152fb7d8b8eea986cc4",
+    "url": "./index.html"
+  }
+];
